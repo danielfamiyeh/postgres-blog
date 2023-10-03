f64383bab550ea975ebf407ef8b1536b3962d6e3
Daniel Famiyeh <dfamiyeh@abcam.com>
2023-10-03 15:37:13 +0100

    Bootstrap server

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+import cors from 'cors';
+import express from 'express';
+
+import { router } from './router';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`)
+})
